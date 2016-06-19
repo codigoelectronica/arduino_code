04f344650a9ddb4dff282b13ed4636b56ab49d23
Oscar Fernandez Alzate <oscarfdzalz@gmail.com>
2016-06-18 20:03:47 -0500

    Se crea proyecto de lcd nokia 5110

---
+++ arduino-lcd-nokia-5110/arduino-lcd-nokia-5110.ino
@@ -0,0 +1,45 @@
+//Se importan las liberias
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+// Conexión de los pines del lcd al display
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+void setup() {
+  Serial.begin(9600);
+  //Se inicializa el lcd
+  display.begin();
+  //Se setea el contraste del lcd
+  display.setContrast(60);
+}
+
+void loop() {
+  //Limpiamos el display
+  display.clearDisplay();
+  //Establecemos el tamaño del text
+  display.setTextSize(1);
+  //Establecemos el color del display
+  display.setTextColor(BLACK);
+  //Se pocisiona el cursor
+  display.setCursor(0,0);
+  //Se escribe
+  display.println("Hola Mundo");
+  //Se muestra la información
+  display.display();
+  delay(2000);
+}
