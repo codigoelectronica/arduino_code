8d8117b28c129b3dda85769cd849f1f012893e74
codigoelectronica <codigoelectronica@gmail.com>
2019-04-17 17:09:24 -0500

    Actualización de código sensor ultrasónico

--- hc-sr04/hc-sr04.ino
+++ hc-sr04/hc-sr04.ino
@@ -1,3 +1,7 @@
+//Inicializamos el lcd
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 //Se declaran los pines para el sensor
 int const TRIG = 9;
 int const ECHO = 8;
@@ -8,6 +12,7 @@ float tiempo;
 
 void setup(){
   Serial.begin(9600);
+  lcd.begin(16, 2);
   //Activación del pin como salida: para el pulso ultrasónico
   pinMode(TRIG, OUTPUT); 
   //Activación del pin como entrada: tiempo del rebote del ultrasonido
@@ -15,25 +20,25 @@ void setup(){
 }
 
 void loop(){
-  //Por cuestión de estabilización del sensor
   digitalWrite(TRIG,LOW); 
   delayMicroseconds(5);
-  //envío del pulso ultrasónico
+  //Envío del pulso ultrasónico
   digitalWrite(TRIG, HIGH); 
   delayMicroseconds(10);
-  /* Función para medir la longitud del pulso entrante. 
-  Mide el tiempo que transcurrido entre el envío
-  del pulso ultrasónico y cuando el sensor recibe el rebote, e
-  s decir: desde que el pin 12 empieza a recibir el rebote, HIGH, hasta que
-  deja de hacerlo, LOW, la longitud del pulso entrante*/
-  tiempo=pulseIn(ECHO, HIGH); 
-  /*fórmula para calcular la distancia obteniendo un valor entero*/
-  /*Monitorización en centímetros por el monitor serial*/
-  distancia= int(0.017*tiempo); 
+
+  //Obtenemos el tiempo de rebote
+  tiempo = pulseIn(ECHO, HIGH); 
+
+  //Calculamos la distancia
+  distancia= tiempo / 2 / 29.1;
   Serial.print("Distancia ");
   Serial.print(distancia);
   Serial.print(" cm");
   Serial.println("");
+  lcd.clear();
+  lcd.print("Distancia");
+  lcd.setCursor(0,1);
+  lcd.print(distancia);
   delay(1000);
 }
   
